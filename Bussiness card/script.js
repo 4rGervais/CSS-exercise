fce9e5ac861eebfee54d669951451c9a82532f9c
4rGervais <gervaisniyonshuti@gmail.com>
2024-11-27 14:32:07 +0200

    Bussiness card & Van life

---
+++ Bussiness card/script.js
@@ -0,0 +1,9 @@
+var num = document.getElementById('add')
+// document.title = 'Bussiness card'
+// num.style.color = 'red'
+// num.innerHTML = 'Kaabayee'
+// var footer = document.getElementById('footer')
+// footer.style.marginTop = '-22px'
+// var y = parseInt(num.textContent)
+
+// num.innerHTML = Date()
