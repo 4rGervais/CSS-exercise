4cb3d211e8809a0038d391d2da507bea40e5ebfe
4rGervais <gervaisniyonshuti@gmail.com>
2024-11-29 18:13:24 +0200

    added script for exercises

--- Bussiness card/script.js
+++ Bussiness card/script.js
@@ -1,9 +1,11 @@
 var num = document.getElementById('add')
+// document.body.style.backgroundColor = 'gray'
 // document.title = 'Bussiness card'
 // num.style.color = 'red'
 // num.innerHTML = 'Kaabayee'
 // var footer = document.getElementById('footer')
 // footer.style.marginTop = '-22px'
-// var y = parseInt(num.textContent)
+var y = parseInt(num.textContent)
 
-// num.innerHTML = Date()
+num.innerHTML = y+4
+// console.log(document.querySelectorAll("p.mn")[0])
